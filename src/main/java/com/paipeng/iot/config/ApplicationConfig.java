026853438bfa1670ec73a40ec8b1f0898d65e7e2
Pai Peng <sipaipv6@gmail.com>
2023-10-12 11:30:16 +0800

    runable

---
+++ src/main/java/com/paipeng/iot/config/ApplicationConfig.java
@@ -0,0 +1,29 @@
+package com.paipeng.iot.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+@Configuration
+@ConfigurationProperties()
+public class ApplicationConfig {
+    @Value("${server.name}")
+    private String serverName;
+
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasenames("messages/string");
+        source.setUseCodeAsDefaultMessage(true);
+
+        return source;
+    }
+
+    public String getServerName() {
+        return serverName;
+    }
+}
