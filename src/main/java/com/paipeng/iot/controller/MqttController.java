5ffecc88e5f25543e58210c95b849b15b0de3c13
Pai Peng <sipaipv6@gmail.com>
2023-10-25 05:35:59 +0800

    add test api for ping a given udid

--- src/main/java/com/paipeng/iot/controller/MqttController.java
+++ src/main/java/com/paipeng/iot/controller/MqttController.java
@@ -1,14 +1,20 @@
 package com.paipeng.iot.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.paipeng.iot.entity.User;
 import com.paipeng.iot.mqtt.gateway.MqttGateway;
+import com.paipeng.iot.mqtt.model.CPIOTPing;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/mqtts")
 public class MqttController {
+    private final Logger logger = LogManager.getLogger(this.getClass().getName());
     @Autowired
     private MqttGateway mqttGateway;
 
@@ -29,5 +35,23 @@ public class MqttController {
     public String sendMsg(String data, String topic) {
         mqttGateway.sendToMqtt(data, topic);
         return "success";
+    }
+
+
+    @GetMapping(value = "/ping/{udid}", produces = {"application/json;charset=UTF-8"})
+    public void testMqttPing(@NotNull @PathVariable("udid") String udid) {
+        CPIOTPing cpiotPing = new CPIOTPing();
+
+        cpiotPing.setUdid(udid);
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String topic = "CP_IOT/" + udid + "/PING";
+            String data = objectMapper.writeValueAsString(cpiotPing);
+            logger.info("sendToMqtt topic: " + topic);
+            logger.info("sendToMqtt data: " + data);
+            mqttGateway.sendToMqtt(data, topic);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
