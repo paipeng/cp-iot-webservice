29fe4ca47af750750abae3bc86f21b0c92288439
Pai Peng <sipaipv6@gmail.com>
2023-10-29 09:39:29 +0800

    use own font

--- src/main/java/com/paipeng/iot/util/Font2ImageUtil.java
+++ src/main/java/com/paipeng/iot/util/Font2ImageUtil.java
@@ -5,35 +5,44 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Font2ImageUtil {
-    public static byte[] text2Image(String text) {
+    private static final String fontName = "fonts/Alibaba-PuHuiTi-Regular.ttf";
+
+    public static byte[] text2Image(String text) throws IOException, FontFormatException {
         BufferedImage img = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2d = img.createGraphics();
         Font font = new Font("Arial", Font.PLAIN, 48);
+
+        //String fontPath = Font2ImageUtil.class.getClassLoader().getResource(fontName).getPath();
+        InputStream is = Font2ImageUtil.class.getClassLoader().getResourceAsStream(fontName);
+        font = Font.createFont(Font.TRUETYPE_FONT, is).deriveFont(24f);
+
         g2d.setFont(font);
         FontMetrics fm = g2d.getFontMetrics();
         int width = fm.stringWidth(text);
         int height = fm.getHeight();
         g2d.dispose();
 
-        img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        img = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_BINARY);
         g2d = img.createGraphics();
         g2d.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
+        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_OFF);
         g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
-        g2d.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
+        g2d.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_DISABLE);
         g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
         g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
         g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
         g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
         g2d.setFont(font);
         fm = g2d.getFontMetrics();
-        g2d.setColor(Color.BLACK);
+        g2d.setColor(Color.WHITE);
         g2d.drawString(text, 0, fm.getAscent());
         g2d.dispose();
         try {
-            ImageIO.write(img, "png", new File("Text.png"));
+            ImageIO.write(img, "bmp", new File("Text.bmp"));
         } catch (IOException ex) {
             ex.printStackTrace();
         }
--- src/test/java/com/paipeng/iot/util/Font2ImageUtilTests.java
+++ src/test/java/com/paipeng/iot/util/Font2ImageUtilTests.java
@@ -3,10 +3,13 @@ package com.paipeng.iot.util;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+import java.io.IOException;
+
 public class Font2ImageUtilTests {
     @Test
-    void text2Image() {
-        String text = "你好";
+    void text2Image() throws IOException, FontFormatException {
+        String text = "你好鹏龘";
         byte[] data = Font2ImageUtil.text2Image(text);
         Assertions.assertNotNull(data);
     }
